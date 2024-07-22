559e127a3c70f2b742b31aa5f84af0149637c148
sai6855 <fayazceg@gmail.com>
2024-07-22 13:17:44 +0530

    refactor getChildRef

--- packages/mui-material/src/utils/getChildRef.js
+++ packages/mui-material/src/utils/getChildRef.js
@@ -1,8 +1,17 @@
 import * as React from 'react';
 
 export default function getChildRef(child) {
-  if (!child || !React.isValidElement(child)) {
-    return null;
+
+  if (process.env.NODE_ENV !== 'production' ) {
+    if(!React.isValidElement(child)){
+      console.error(
+        [
+          "MUI: getChildRef doesn't accept array as a child.",
+          'Consider providing a single element instead.',
+        ].join('\n'),
+      );
+      return null;
+    }
   }
   // 'ref' is passed as prop in React 19, whereas 'ref' is directly attached to children in React 18
   // below check is to ensure 'ref' is accessible in both cases
