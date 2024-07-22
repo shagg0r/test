13436326565129b6bb8619a783a55c02d6ae503a
sai6855 <fayazceg@gmail.com>
2024-07-22 15:33:40 +0530

    prettier

--- packages/mui-material/src/utils/getChildRef.js
+++ packages/mui-material/src/utils/getChildRef.js
@@ -4,9 +4,7 @@ import * as React from 'react';
 export default function getChildRef(child) {
   if (process.env.NODE_ENV !== 'production') {
     if (!React.isValidElement(child)) {
-      throw new MuiError(
-       'MUI: getChildRef expected to receive a single React element child.'
-      );
+      throw new MuiError('MUI: getChildRef expected to receive a single React element child.');
     }
   }
   // 'ref' is passed as prop in React 19, whereas 'ref' is directly attached to children in React 18
