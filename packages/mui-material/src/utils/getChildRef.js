97671079ce89ab83c3ac0676722ff796e957ca7c
sai6855 <fayazceg@gmail.com>
2024-08-09 12:46:20 +0530

    move logic

--- packages/mui-material/src/utils/getChildRef.js
+++
@@ -1,5 +0,0 @@
-export default function getChildRef(child) {
-  // 'ref' is passed as prop in React 19, whereas 'ref' is directly attached to children in React 18
-  // below check is to ensure 'ref' is accessible in both cases
-  return child.props.propertyIsEnumerable('ref') ? child.props.ref : child.ref;
-}
